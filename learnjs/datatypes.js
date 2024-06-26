08e5ab100c227b0b1376495a57ff61e909ab8650
Akprogrammer-mnnit <144123050+Akprogrammer-mnnit@users.noreply.github.com>
2024-06-26 06:52:39 +0000

    datatypes and variables

---
+++ learnjs/datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+let name = "Ayan"
+let age = "19"
+let pass = true
+console.log(typeof null) // Object
+console.log(typeof undefined)// Undefined
---
+++ learnjs/variable.js
@@ -0,0 +1,4 @@
+const accountId =  12234
+let accountEmail = "random@gmail.com"
+let accountCity;
+console.table([accountId, accountEmail, accountCity])
