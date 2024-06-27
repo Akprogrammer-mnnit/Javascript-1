d96584d0084e1e566ed84d73dd21f7531f62dd09
Akprogrammer-mnnit <144123050+Akprogrammer-mnnit@users.noreply.github.com>
2024-06-27 06:15:32 +0000

    string

---
+++ learnjs/string.js
@@ -0,0 +1,24 @@
+const name = new String('Hello World')
+
+
+// let index=3
+// console.log(`At index ${index} value is ${name.at(index)}`)
+
+// let index=3
+// console.log(`character at the index ${index} is ${name.charAt(index)}`)
+
+// let index=3
+// console.log(`character code ${name.charCodeAt(index)} is equal to ${name.charAt(index)}`)
+
+// const anothername = new String(' This is me AK')
+// console.log(`String after concatination is ${name.concat(anothername)}`)
+
+// console.log(`${name.endsWith('r',9)}`)
+
+// const sentence = new String('This is me AK learning JavaScript String Functions')
+// const word = new String('javascript')
+// console.log(`The word ${word} ${sentence.includes(word)?'is' : 'is not'} in the sentence`);
+
+
+// console.log(name.indexOf('l',5));
+
