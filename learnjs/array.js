d6dc3d6aad22f06ae31099c18a36745e9d2611f8
Akprogrammer-mnnit <144123050+Akprogrammer-mnnit@users.noreply.github.com>
2024-06-28 05:59:00 +0000

    arrays and objects

---
+++ learnjs/array.js
@@ -0,0 +1,5 @@
+const arr = [1,2,3,4,"hello"]
+const newarr = [4,5,2,5,{name:"Ayan", id:1132441}]
+const combinedarray = arr.push(12)
+
+console.log(arr);
---
+++ learnjs/objects.js
@@ -0,0 +1,9 @@
+const gen = Symbol("Gender")
+const user  = {
+    name: "Hitesh",
+    id: 123415,
+    gen: "m",
+    email: "hitesh@gmail.com",
+    isLoggedIn: true
+}
+console.log(user);
