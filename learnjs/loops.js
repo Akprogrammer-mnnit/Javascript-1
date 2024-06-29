979ecb072c0c2d21fef71846737254ff71526521
Akprogrammer-mnnit <144123050+Akprogrammer-mnnit@users.noreply.github.com>
2024-06-29 08:09:58 +0000

    loops

---
+++ learnjs/loops.js
@@ -0,0 +1,38 @@
+const arr = [1,2,3,4,5,6]
+const obj = {
+    name: "Ayan",
+    isLoggedIn: true,
+    city: "Delhi"
+}
+// for (const val of arr) {
+//     console.log(val);
+// }
+// for (const key in obj) {
+//     console.log(`${key} ====>  ${obj[key]}`);
+// }
+
+// arr.forEach(function (item) {
+//     console.log(item)
+// })
+// arr.forEach((item,index,arrat) => {
+//     console.log(`${item} ${index}`,arrat)
+// })
+
+// const numsarray = arr.filter((items) => items>3)
+// console.log(numsarray);
+// const numsarray = arr.map((item) => item>3)
+// console.log(numsarray);
+
+
+// const numsarray = arr.map((item) => item+10).filter((item) => item>13).map((item)=> item*10)
+// console.log(numsarray);
+// console.log(arr);
+
+// const sum = arr.reduce((acc,curr) => acc+curr , 0)
+// console.log(sum);
+
+
+// const sum = arr.reduce(function(acc,curr) {
+//     return acc+curr
+// }, 0)
+// console.log(sum);
